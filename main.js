0401c8fed6c75e1c53912df7208c22f772003ca0
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-04-23 01:46:04 +0200

    content completed
    Contenu Terminé, plus qu'à faire le responsive

--- main.js
+++ main.js
@@ -32,3 +32,51 @@ btnContact.addEventListener('click', function () {
   projets.style.display = "none";
   contact.style.display = "block";
 })
+
+// PROJETS
+const myProjets = [
+  {
+    id: 1,
+    pict: "/assets/picts/SpiritsGamerSite.png",
+    picth: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    web: "https://spirits-gamer.vercel.app/main",
+    source: "https://github.com/Matfen2/Spirits-Gamer",
+  },
+  {
+    id: 2,
+    pict: "/assets/picts/soulsHeroesSite.png",
+    picth: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    web: "https://souls-heroes.vercel.app/main",
+    source: "https://github.com/Matfen2/SoulsHeroesBackend/tree/master",
+  }, 
+  {
+    id: 3,
+    pict: "/assets/picts/tlouSite.png",
+    picth: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    web: "https://the-last-of-us-orcin.vercel.app/main",
+    source: "https://github.com/Matfen2/The-Last-Of-Us",
+  }
+];
+
+document.addEventListener("DOMContentLoaded", function() {
+  afficherProjet(myProjets[0]);
+});
+
+const logos = document.querySelectorAll('.logo');
+
+logos.forEach(logo => {
+  logo.addEventListener('click', function () {
+    const projetId = parseInt(logo.dataset.id);
+    const projet = myProjets.find(projet => projet.id === projetId);
+    afficherProjet(projet);
+  });
+});
+
+function afficherProjet(projet) {
+  document.getElementById('projetImage').src = projet.pict;
+  document.getElementById('projetPitch').textContent = projet.picth;
+  document.getElementById('projetWebLink').href = projet.web;
+  document.getElementById('projetSourceLink').href = projet.source;
+}
+
+
