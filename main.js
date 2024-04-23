4356b64ef61213ff1ec4d7ea3ea29c8c8e7044fa
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-04-23 20:04:32 +0200

    final version
    responsive over, comments of people waiting

--- main.js
+++ main.js
@@ -38,36 +38,36 @@ const myProjets = [
   {
     id: 1,
     pict: "/assets/picts/SpiritsGamerSite.png",
-    picth: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    picth: "Un site de recherche et d'achat de jeux vidéo",
     web: "https://spirits-gamer.vercel.app/main",
     source: "https://github.com/Matfen2/Spirits-Gamer",
   },
   {
     id: 2,
     pict: "/assets/picts/soulsHeroesSite.png",
-    picth: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    picth: "Un site dédié à la recherche et à l'achat de comics mettant en scène des super-héros.",
     web: "https://souls-heroes.vercel.app/main",
     source: "https://github.com/Matfen2/SoulsHeroesBackend/tree/master",
   }, 
   {
     id: 3,
     pict: "/assets/picts/tlouSite.png",
-    picth: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore",
+    picth: "Un site basé sur l'univers de l'une de mes sagas de jeu vidéo préférée.",
     web: "https://the-last-of-us-orcin.vercel.app/main",
     source: "https://github.com/Matfen2/The-Last-Of-Us",
   }
 ];
 
 document.addEventListener("DOMContentLoaded", function() {
-  afficherProjet(myProjets[0]);
+  afficherProjet(myProjets[0]); // Afficher le premier élément du tableau myProjets
 });
 
 const logos = document.querySelectorAll('.logo');
 
 logos.forEach(logo => {
   logo.addEventListener('click', function () {
-    const projetId = parseInt(logo.dataset.id);
-    const projet = myProjets.find(projet => projet.id === projetId);
+    const projetId = parseInt(logo.dataset.id); // Sélectionner les logos avec parseInt
+    const projet = myProjets.find(projet => projet.id === projetId); // Trouver le projet correspondant avec la technique array.find
     afficherProjet(projet);
   });
 });
@@ -78,5 +78,40 @@ function afficherProjet(projet) {
   document.getElementById('projetWebLink').href = projet.web;
   document.getElementById('projetSourceLink').href = projet.source;
 }
+
+// CONTACT
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêcher l'envoi du formulaire par défaut
+
+  // Récupérer les valeurs des champs
+  const userValue = user.value;
+  const emailValue = email.value;
+  const messageValue = message.value;
+
+  // Validation simple des champs
+  if (!userValue || !emailValue || !messageValue) {
+    alert("Veuillez remplir tous les champs.");
+    return;
+  }
+
+  // Validation de l'adresse email
+  if (!isValidEmail(emailValue)) {
+    alert("Veuillez saisir une adresse email valide.");
+    return;
+  }
+
+  // Envoyer le message
+  alert(userValue + ', votre message a été envoyé avec succès.');
+});
+
+// Fonction pour valider l'adresse email
+function isValidEmail(email) {
+  // Expression régulière pour vérifier le format de l'adresse email
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
 
 
