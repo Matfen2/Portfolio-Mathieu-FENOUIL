61f49349e77f668df967cd198fa38b24cc8812f4
EyesEagle9 <110094556+EyesEagle9@users.noreply.github.com>
2024-04-27 04:05:21 +0200

    new version of portfolio
    new structure of my portfolio :           navbar, main

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
-const info = document.getElementById('btnInfo');
-const identify = document.querySelector('.identify');
-const show = document.querySelector('.showInfo');
-
-info.addEventListener('click', function () {
-  identify.style.display = "none";
-  show.style.display = "block";
-})
-
+const btnMain = document.getElementById('btnMain');
 const btnAbout = document.getElementById('btnAbout');
 const btnProjets = document.getElementById('btnProjets');
 const btnContact = document.getElementById('btnContact');
 
-const about = document.querySelector('.about');
-const projets = document.querySelector('.projets');
-const contact = document.querySelector('.contact');
+const main = document.getElementById('main');
+const about = document.getElementById('about');
+const projets = document.getElementById('projets');
+const contact = document.getElementById('contact');
+
+btnMain.addEventListener('click', function () {
+  main.style.display = "block";
+  about.style.display = "none";
+  projets.style.display = "none";
+  contact.style.display = "none";
+})
 
 btnAbout.addEventListener('click', function () {
+  main.style.display = "none";
   about.style.display = "block";
   projets.style.display = "none";
   contact.style.display = "none";
 })
 
 btnProjets.addEventListener('click', function () {
+  main.style.display = "none";
   about.style.display = "none";
   projets.style.display = "block";
   contact.style.display = "none";
 })
 
 btnContact.addEventListener('click', function () {
+  main.style.display = "none";
   about.style.display = "none";
   projets.style.display = "none";
   contact.style.display = "block";
@@ -59,15 +62,15 @@ const myProjets = [
 ];
 
 document.addEventListener("DOMContentLoaded", function() {
-  afficherProjet(myProjets[0]); // Afficher le premier élément du tableau myProjets
+  afficherProjet(myProjets[0]); 
 });
 
 const logos = document.querySelectorAll('.logo');
 
 logos.forEach(logo => {
   logo.addEventListener('click', function () {
-    const projetId = parseInt(logo.dataset.id); // Sélectionner les logos avec parseInt
-    const projet = myProjets.find(projet => projet.id === projetId); // Trouver le projet correspondant avec la technique array.find
+    const projetId = parseInt(logo.dataset.id); 
+    const projet = myProjets.find(projet => projet.id === projetId); 
     afficherProjet(projet);
   });
 });
@@ -78,40 +81,3 @@ function afficherProjet(projet) {
   document.getElementById('projetWebLink').href = projet.web;
   document.getElementById('projetSourceLink').href = projet.source;
 }
-
-// CONTACT
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Empêcher l'envoi du formulaire par défaut
-
-  // Récupérer les valeurs des champs
-  const userValue = user.value;
-  const emailValue = email.value;
-  const messageValue = message.value;
-
-  // Validation simple des champs
-  if (!userValue || !emailValue || !messageValue) {
-    alert("Veuillez remplir tous les champs.");
-    return;
-  }
-
-  // Validation de l'adresse email
-  if (!isValidEmail(emailValue)) {
-    alert("Veuillez saisir une adresse email valide.");
-    return;
-  }
-
-  // Envoyer le message
-  alert(userValue + ', votre message a été envoyé avec succès.');
-});
-
-// Fonction pour valider l'adresse email
-function isValidEmail(email) {
-  // Expression régulière pour vérifier le format de l'adresse email
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return emailRegex.test(email);
-}
-
-
-
